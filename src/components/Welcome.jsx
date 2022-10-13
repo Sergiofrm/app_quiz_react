c663b2d664288c184b7868a17ec09bcee065f825
sergiofrm <serginho.felipe@hotmail.com>
2022-10-13 10:17:43 -0300

    Questions Finalizados

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -10,7 +10,7 @@ const Welcome = () => {
 
   return (
     <div id="welcome">
-      <h2>Em fase de teste ...</h2>
+      <h2>Bem-vindos...</h2>
       <p>Clique no botão abaixo para começar:</p>
       <button onClick={() => dispatch({ type: "CHANGE_STAGE" })}>
         Iniciar
